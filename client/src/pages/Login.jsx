4c0271c8681b3dd514f8cd189cca7cc030c083ad
Maria Padilla <maria.pad.mar@gmail.com>
2024-03-13 16:51:30 +0100

    Create token from the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
-// Any component that wants to ‘consume’ / be aware of authContext 
-// just needs to import the useAuth 
-// Example: import useAuth from “../hooks/useAuth”; at the top of the Login.jsx file. 
-// Then, instantiate it by creating a variable 
+// Any component that wants to ‘consume’ / be aware of authContext
+// just needs to import the useAuth
+// Example: import useAuth from “../hooks/useAuth”; at the top of the Login.jsx file.
+// Then, instantiate it by creating a variable
 // const {isLoggedIn} = useAuth().
 // From there, you can then use isLoggedIn to show / hide pages, like this:
 // {isLoggedIn ? (
@@ -13,9 +14,41 @@ import React from "react";
 // ) : (
 // <div>
 //    <div className= “alert”> You are logged out</div>
-// What we’re saying in the above example is “Is logged in true? If so, show me X (div and whatever it contains). 
+// What we’re saying in the above example is “Is logged in true? If so, show me X (div and whatever it contains).
 // Otherwise, show me Y (div and whatever it contains)”
 // BBM
-export default function Login () {
-    
+export default function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  async function login() {
+    try {
+      const { data } = await axios.post("/api/auth/login", credentials);
+      localStorage.setItem("token", data.token);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  return (
+    <div>
+      <input
+        placeholder="username"
+        value={credentials.username}
+        onChange={(e) =>
+          setCredentials({ ...credentials, username: e.target.value })
+        }
+      ></input>
+      <input
+        placeholder="password"
+        type="password"
+        value={credentials.password}
+        onChange={(e) =>
+          setCredentials({ ...credentials, password: e.target.value })
+        }
+      ></input>
+      <button onClick={login}>Login</button>
+    </div>
+  );
 }
