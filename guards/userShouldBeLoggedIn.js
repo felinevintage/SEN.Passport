fd4c13179422da5b68663ccc3234ae83f1633cc8
Maria Padilla <maria.pad.mar@gmail.com>
2024-03-14 13:16:24 +0100

    Get all the assessments

--- guards/userShouldBeLoggedIn.js
+++ guards/userShouldBeLoggedIn.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const supersecret = process.env.SUPER_SECRET;
 
 function userShouldBeLoggedIn(req, res, next) {
-  const token = req.headers["authorization"].replace(/^Bearer\s/, "");
+  const token = req.headers["authorization"]?.replace(/^Bearer\s/, "");
   if (!token) {
     res.status(401).send({ message: "Please provide a token" });
   } else {
--- routes/assess.js
+++ routes/assess.js
@@ -1,10 +1,37 @@
 var express = require("express");
 var router = express.Router();
-var models = require("../models")
+var models = require("../models");
+const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send({ title: "Express" });
+  res.send({ title: "Assessments" });
 });
+
+/* GET all assessments of one child */
+router.get("/all", userShouldBeLoggedIn, async (req, res) => {
+  const childId = req.params.id;
+  try {
+    await models.Assessments.findAll({
+      where: {
+        childId,
+      },
+    });
+    res.send("Success");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/* POST assessment */
+router.post("/", async (req, res) => {
+  const { assessment_type, date, results_doc, childId } = req.body;
+});
+
+/* GET assessments by id */
+router.get("/:id", async (req, res) => {});
+
+/* DELETE assessments by id */
+router.delete("/:id", async (req, res) => {});
 
 module.exports = router;
