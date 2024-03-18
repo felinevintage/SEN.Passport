5b3e9bdeee4e1f0287e7ca63f911c56f0a06a38f
Maria Padilla <maria.pad.mar@gmail.com>
2024-03-18 11:54:18 +0100

    Add document from the app

--- client/src/pages/Documents.jsx
+++ client/src/pages/Documents.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 
 export default function Documents() {
   const [documents, setDocuments] = useState([]);
-  //const [child, setChild] = useState();
+  const [newDocs, setNewDocs] = useState(null);
+  const [error, setError] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
@@ -30,6 +31,40 @@ export default function Documents() {
     }
   }
 
+  const handleFileChange = (e) => {
+    setNewDocs(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!newDocs) {
+      setError("Please select a file.");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", newDocs);
+
+    try {
+      const response = await fetch(`/api/children/${id}/documents`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          // "Content-Type": "multipart/form-data",
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        getDocuments();
+        // console.log(data);
+      } else {
+        console.log("Failed to add document");
+      }
+    } catch (error) {
+      console.error("Error updating document:", error);
+    }
+  };
+
   return (
     <div className="container p-4 flex justify-center items-center h-screen">
       <div className="text-center bg-slate-300 p-8 rounded-md">
@@ -43,6 +78,17 @@ export default function Documents() {
             {document.doc_name}
           </div>
         ))}
+      </div>
+      <div>
+        New document
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="fileInput">Choose file:</label>
+            <input type="file" id="fileInput" onChange={handleFileChange} />
+          </div>
+          {error && <p>{error}</p>}
+          <button>Update Document</button>
+        </form>
       </div>
     </div>
   );
