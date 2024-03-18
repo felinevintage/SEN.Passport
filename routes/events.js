55b01fbfedc1656dada832e25d87a8a038b9fad0
ShonaPugh <shonapugh@gmail.com>
2024-03-18 12:50:32 +0000

    Add events page complete

--- routes/events.js
+++ routes/events.js
@@ -1,10 +1,38 @@
 var express = require("express");
 var router = express.Router();
-var models = require("../models")
+var models = require("../models");
+const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
+const childMustExist = require("../guards/childMustExist");
+const mustHaveChildPermission = require("../guards/mustHaveChildPermission");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send({ title: "Express" });
 });
+
+router.post("/:id", 
+[userShouldBeLoggedIn,
+childMustExist,
+mustHaveChildPermission], async function (req, res) {
+  const {child} = req
+  const {
+    event_type, 
+    date, 
+    time, 
+    location
+  } = req.body;
+
+  try {
+    const event = await child.createEvent({
+      event_type: event_type,
+      date: date,
+      time: time,
+      location: location
+    });
+    res.send(event)
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
 
 module.exports = router;
