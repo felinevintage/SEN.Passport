877e2c45c5d7add9339ade75e6ff4a74fc710cb6
Maria Padilla <maria.pad.mar@gmail.com>
2024-03-19 09:37:26 +0100

    Add back button documents page

--- client/src/pages/Documents.jsx
+++ client/src/pages/Documents.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Button from "../components/Button";
 import Modal from "../components/Modal";
@@ -10,6 +10,7 @@ export default function Documents() {
   const [error, setError] = useState(null);
   const [selectedDocument, setSelectedDocument] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getDocuments();
@@ -70,6 +71,25 @@ export default function Documents() {
         {" "}
         <h1 className="font-bold text-lg">Documents</h1>
       </div>
+      <button
+        className="absolute top-0 right-0 px-4 py-2 bg-slate-100 rounded-md"
+        onClick={() => navigate(`/children/${id}`)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+          />
+        </svg>
+      </button>
       <div className="grid grid-cols-2 gap-4">
         <div className="col-span-1 flex justify-center my-10">
           <table className="table-auto bg-slate-300 p-8 rounded-md">
